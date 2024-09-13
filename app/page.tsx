7c13248fe567c15ba5965266b509d58a0d211fd8
r_hinojosa <r_hinojosa@ucsb.edu>
2024-09-13 16:31:09 -0700

    Working on search input

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import AddMaterialsForm from "@/components/AddMaterialsForm";
+import SearchCombobox from "@/components/SearchCombobox";
 
 export default async function Page() {
   return (
     <div className="h-full w-full flex flex-col justify-between">
-      <h1 className="text-textColor-400 font-bold text-2xl mt-5">Dashboard</h1>
+      {/* <h1 className="text-textColor-400 font-bold text-2xl mt-5">Dashboard</h1>
       <div className="h-[91vh] mt-8 bg-white rounded-2xl p-20">
         <AddMaterialsForm />
-      </div>
+      </div> */}
+      <SearchCombobox />
     </div>
   );
 }
